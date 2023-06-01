8e209a276f3370f00dde3836299a6ce25a6c7d12
Guilherme Vezula <vezula18@gmail.com>
2023-06-01 10:29:49 -0300

    add colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          dark: "#1A1E2C",
+          medium: "#8E94A7",
+          light: "#E7E9F0",
+          stroke: "#EBEBF9",
+        },
+        blue: {
+          dark: "#0346F2",
+          medium: "#04C3FF",
+          light: "#F2F5FE",
+        },
+        orange: {
+          dark: "#F2994A",
+          light: "#EBD7D7",
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
