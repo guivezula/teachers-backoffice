9c2f1a240fefb3e911e85aac3b9a961bfa8c0960
Guilherme Vezula <vezula18@gmail.com>
2023-06-04 12:47:42 -0300

    add date utils

---
+++ src/components/class-item/Date.utils.ts
@@ -0,0 +1,56 @@
+const months = [
+  "janeiro",
+  "fevereiro",
+  "março",
+  "abril",
+  "maio",
+  "junho",
+  "julho",
+  "agosto",
+  "setembro",
+  "outubro",
+  "novembro",
+  "dezembro",
+];
+
+const addZero = (n: number): string => {
+  if (n <= 9) return `0${n}`;
+  else return n.toFixed(0);
+};
+
+const diffDays = (date: Date) => {
+  const today = new Date();
+  const timeDiff = date.getTime() - today.getTime();
+  return Math.abs(Math.ceil(timeDiff / (1000 * 3600 * 24)));
+};
+
+const getDate = (date: Date) => {
+  return `${addZero(date.getDate())} ${
+    months[date.getMonth()]
+  } ${date.getFullYear()}`;
+};
+
+const formatedDate = (originDate: string) => {
+  const splited = originDate.split("-").map((n) => Number(n));
+  const date = new Date(splited[0], splited[1] - 1, splited[2]);
+  const diff = diffDays(date);
+  console.log(diff);
+
+  if (diff === 0) {
+    return "hoje";
+  }
+
+  if (diff === 1) {
+    return "ontém";
+  }
+
+  if (diff > 1 && diff <= 3) {
+    return `${diff} dias atrás`;
+  }
+
+  return getDate(date);
+};
+
+export const DateUtils = {
+  formatedDate,
+};
