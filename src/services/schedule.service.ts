642258ace497763c69e3e3af96c753d392d1feae
Guilherme Vezula <vezula18@gmail.com>
2023-06-05 10:41:09 -0300

    change mock url to api url

--- src/services/schedule.service.ts
+++ src/services/schedule.service.ts
@@ -1,8 +1,8 @@
 import { Schedule } from "../models/schedule.models";
 
 const getAll = async (): Promise<Schedule[]> =>
-  await fetch(`${process.env.REACT_APP_API_URL}/schedules`).then((response) =>
-    response.json()
+  await fetch(`${process.env.REACT_APP_API_URL}/schedules/today`).then(
+    (response) => response.json()
   );
 
 export const SchedulesService = {
